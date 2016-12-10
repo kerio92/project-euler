7e1c769e247540b60f69227bf6039c125670b72f
Edoardo Spadolini <kerio00@gmail.com>
2016-12-10 18:35:15 +0100

    fix your style or riot

--- libs/primes.py
+++ libs/primes.py
@@ -1,20 +1,19 @@
-import math
-def prime(n):
+def primes(n):
     p = [True] * n
-    for i in range(2,n):
+    for i in range(2, n):
         if not p[i]: continue
         for j in range(i+i, n, i):
             p[j] = False
         yield i
-def factorOutFactory(n):
-    for d in primes(n//2+1):
-        if n==1:
+
+def factors(n):
+    for d in primes(n//2 + 1):
+        if n == 1:
             return
         while n % d == 0:
             yield d
             n //= d
-
-def factorOut(num):
-    return list(factorOutFactory(n))
-
-
+    yield n
+            
+def factorOut(n):
+    return list(factors(n))
